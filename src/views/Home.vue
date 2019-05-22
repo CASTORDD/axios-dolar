1c97f91462a67153679b9b3ee07dac9cb7bbfae2
castordd <castord.d@gmail.com>
2019-05-22 17:43:13 -0300

    Ajustes do projeto

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <v-layout>
+    <v-flex>
+      <h1>Hola mundo</h1>
+    </v-flex>
+    <v-flex>
+      <h1>Hola mundo</h1>
+    </v-flex>
+    <v-flex>
+      <h1>Hola mundo</h1>
+    </v-flex>
+  </v-layout>
+  <v-layout>
+    <v-flex>
+      <h1>Hola mundo</h1>
+    </v-flex>
+    <v-flex>
+      <h1>Hola mundo</h1>
+    </v-flex>
+    <v-flex>
+      <h1>Hola mundo</h1>
+    </v-flex>
+  </v-layout>
+</div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
